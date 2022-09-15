1e43b646273d63e02811186e27b149316fe84402
Patricia Eiterer <eitererpatricia@gmail.com>
2022-09-15 18:20:39 -0300

    modified:   tarefa1/index.js modified:   tarefa1/index.ts

--- tarefa1/index.js
+++ tarefa1/index.js
@@ -2,6 +2,6 @@
 function sum(a, b) {
     return a + b;
 }
-let x = 1586;
+let x = 1536;
 let y = 7294;
 console.log(sum(x, y));
--- tarefa1/index.ts
+++ tarefa1/index.ts
@@ -2,7 +2,7 @@ function sum (a:number, b:number) {
     return a+b
 }
 
-let x: number = 1586
+let x: number = 1536
 let y: number = 7294
 
 console.log(sum(x,y))
