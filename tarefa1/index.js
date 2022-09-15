6f4c9c508de1bf2c26d082d9432f1a3a4ccc54bd
Patricia Eiterer <eitererpatricia@gmail.com>
2022-09-15 17:50:10 -0300

    Tarefa da Seção 1

---
+++ tarefa1/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const fisrtNum = 1536;
+const secondNum = 7294;
+function sum(total) {
+    console.log(total);
+}
+sum(fisrtNum + secondNum);
---
+++ tarefa1/index.ts
@@ -0,0 +1,8 @@
+const fisrtNum: number = 1536
+const secondNum: number = 7294
+
+function sum (total:number) {
+    console.log(total)
+}
+
+sum(fisrtNum + secondNum)
